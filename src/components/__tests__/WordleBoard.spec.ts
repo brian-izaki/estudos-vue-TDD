8484cc531bfd3b5051a4ee552495c7e8a8b6991a
brian-izaki <brian.izaki@gmail.com>
2024-11-02 20:50:07 -0300

    removing cluster and magical words

--- src/components/__tests__/WordleBoard.spec.ts
+++ src/components/__tests__/WordleBoard.spec.ts
@@ -2,20 +2,18 @@ import { describe, it, expect } from 'vitest'
 
 import { mount } from '@vue/test-utils'
 import WordleBoard from '../WordleBoard.vue'
+import { VICTORY_MESSAGE } from '@/settings'
 
 describe('WordleBoard', () => {
   test('a victory message appears when the user makes a guess that matches the word of the day', async () => {
-    // arrange
     const wrapper = mount(WordleBoard, {
       props: { wordOfTheDay: 'TESTS' },
     })
 
-    // Act
     const guessInput = wrapper.find('input[type=text]')
     await guessInput.setValue('TESTS')
     await guessInput.trigger('keydown.enter')
 
-    // assert
-    expect(wrapper.text()).toContain('You won!')
+    expect(wrapper.text()).toContain(VICTORY_MESSAGE)
   })
 })
